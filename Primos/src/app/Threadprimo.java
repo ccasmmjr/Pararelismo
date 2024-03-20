47113d767cea60f8f29cbd555253eebe017db9bb
ccasmmjr <84786554+ccasmmjr@users.noreply.github.com>
2024-03-20 16:08:22 -0300

    Update Threadprimo.java

--- Primos/src/app/Threadprimo.java
+++ Primos/src/app/Threadprimo.java
@@ -6,8 +6,8 @@ import pacote.Primo;
 public class Threadprimo {
     public static void main(String[] args)  {
     
-        Primo primo = new Primo("C:/Users/ccasmmjr/Desktop/AtvSistemaDistribuido/Primos/Entrada01.txt");
-
+        Primo primo = new Primo("local onde esta o seu arquivo txt apra leitura");
+     //exemplo C:/Users/seu user/Desktop/AtvSistemaDistribuido/Primos/Entrada01.txt
         for (int i = 0;i< 10;i++){
             
             Thread thread = new Thread(
