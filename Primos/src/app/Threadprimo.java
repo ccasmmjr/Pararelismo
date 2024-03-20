2922db6b11464bb47a91c37dc263da62b4978c7a
ccasmmjr <ccasmmj@gmail.com>
2024-03-20 16:26:26 -0300

    codigo com thred e leitura e interface

--- Primos/src/app/Threadprimo.java
+++ Primos/src/app/Threadprimo.java
@@ -2,18 +2,47 @@ package app;
 
  
 
+import java.util.Scanner;
+
 import pacote.Primo;
 public class Threadprimo {
     public static void main(String[] args)  {
     
-        Primo primo = new Primo("C:/Users/ccasmmjr/Desktop/AtvSistemaDistribuido/Primos/Entrada01.txt");
+        Scanner scan = new Scanner(System.in);
+        Primo primo = new Primo("C:/Users/ccasmmjr/Desktop/AtvSistemaDistribuido/Pararelismo/Primos/Entrada01.txt");
+        int num_thread=0;
+        int escolha=0;
+        int conf=0;
+        System.out.println("Digite 1 ser que 1 thread, 2 se sao 5 e 3 se sao 10\n");
+        System.out.println("Digite quaquer outro numero se quiser encerra \n");
+        escolha=scan.nextInt();
+            switch(escolha){
+                case 1 :
+                num_thread=1;
+                conf=1;
+                break;
+                case 2 :
+                num_thread=5;
+                conf=1;
+                break;
+                case 3 :
+                conf=1;
+                num_thread=10; 
+                break;
+                default :                
+            }
+        
+        if(conf==1){
+            System.out.println("A seguir exibira os numeros primos \n");
 
-        for (int i = 0;i< 10;i++){
+            for (int i = 0;i< num_thread;i++){
             
-            Thread thread = new Thread(
-                () -> primo.verificar(),"thread-"+i
-            );
-            thread.start();
+                Thread thread = new Thread(
+                    () -> primo.verificar(),"thread-"+i
+                );
+                thread.start();
+            }
         }
+        
     }
 }
